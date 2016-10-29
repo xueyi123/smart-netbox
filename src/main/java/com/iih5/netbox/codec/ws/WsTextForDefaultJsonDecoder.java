4306011bb9f5c08cfa9fc82269e883b707a39df2
xueyi123 <qq101388>
2016-10-29 21:48:43 +0800

    New changelist

--- src/main/java/com/iih5/netbox/codec/ws/WsTextForDefaultJsonDecoder.java
+++ src/main/java/com/iih5/netbox/codec/ws/WsTextForDefaultJsonDecoder.java
@@ -6,20 +6,25 @@ import org.apache.log4j.Logger;
 
 import java.util.List;
 
-public class WsTextForDefaultJsonDecoder extends WsTextDecoder{
+public class WsTextForDefaultJsonDecoder extends WsTextDecoder {
     private Logger logger = Logger.getLogger(WsTextForDefaultJsonDecoder.class);
+
     @Override
     public void decode(Channel ctx, String text, List<Object> out) {
-        synchronized (this){
+        out.add(unPack(text));
+    }
+
+    public StringMessage unPack(String text) {
+        synchronized (this) {
             //Text协议定义: "【协议ID】#【加密类型】#【内容】",比如 10001#0#hello world!
             String arr[] = text.split("#", 3);
             short msgId = Short.valueOf(arr[0]);
-            byte encr =Byte.valueOf(arr[1]);
+            byte encr = Byte.valueOf(arr[1]);
             String content = arr[2];
-            StringMessage jm= new StringMessage(msgId);
+            StringMessage jm = new StringMessage(msgId);
             jm.setEncrypt(encr);
             jm.setContent(content);
-            out.add(jm);
+            return jm;
         }
     }
 }
--- src/main/java/com/iih5/netbox/codec/ws/WsTextForDefaultJsonEncoder.java
+++ src/main/java/com/iih5/netbox/codec/ws/WsTextForDefaultJsonEncoder.java
@@ -3,17 +3,18 @@ package com.iih5.netbox.codec.ws;
 import com.iih5.netbox.message.StringMessage;
 import io.netty.channel.Channel;
 
-public class WsTextForDefaultJsonEncoder extends WsTextEncoder{
+public class WsTextForDefaultJsonEncoder extends WsTextEncoder {
 
     @Override
     public void encode(Channel ctx, Object msg, StringBuffer text) {
-       pack(msg, text);
+        pack(msg, text);
     }
-    public void pack( Object msg, StringBuffer text){
-        synchronized (this){
+
+    public void pack(Object msg, StringBuffer text) {
+        synchronized (this) {
             //Text协议定义: "【协议ID】#【加密类型】#【内容】",比如 10001#0#hello world!
-            StringMessage jm= (StringMessage) msg;
-            StringBuffer sb= new StringBuffer();
+            StringMessage jm = (StringMessage) msg;
+            StringBuffer sb = new StringBuffer();
             sb.append(jm.getId());
             sb.append("#");
             sb.append(0);
